f96ee56449cb8080a88ac4bbd64301f3f9e673d4
jonathanmcshane <jonathan.mcshane@yahoo.com>
2024-01-31 11:41:53 +0000

    Added unittests for Maze class

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
 from graphics import Line, Point
 
 class Cell:
-    def __init__(self, window):
+    def __init__(self, window=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
@@ -10,34 +10,39 @@ class Cell:
         self._y1 = None
         self._x2 = None
         self._y2 = None
-        self._window = window
+        self._win = window
 
     def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
         self._y2 = y2
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
-            self._window.draw_line(line)
+            self._win.draw_line(line)
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
-            self._window.draw_line(line)
+            self._win.draw_line(line)
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
-            self._window.draw_line(line)
+            self._win.draw_line(line)
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
-            self._window.draw_line(line)
+            self._win.draw_line(line)
     
     def draw_move(self, to_cell, undo=False):
-        fill_colour = ""
+        if self._win is None:
+            return
+
+        fill_colour = "red"
         if undo:
             fill_colour = "gray"
-        else:
-            fill_colour = "red"
+
         from_point = Point((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2 )
         to_point = Point((to_cell._x1 + to_cell._x2) / 2, (to_cell._y1 + to_cell._y2) / 2 )
         
         line = Line(from_point, to_point)
-        self._window.draw_line(line, fill_colour)
+        self._win.draw_line(line, fill_colour)
--- maze.py
+++ maze.py
@@ -10,7 +10,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win,
+        win=None,
     ):
         self._x1 = x1
         self._y1 = y1
@@ -34,6 +34,8 @@ class Maze:
                 self._draw_cell(i, j)
     
     def _draw_cell(self, i, j):
+        if self._win is None:
+            return
         cell_x1 = self._x1 + (i * self._cell_size_x)
         cell_y1 = self._y1 + (j * self._cell_size_y)
         cell_x2 = cell_x1 + self._cell_size_x
@@ -42,5 +44,7 @@ class Maze:
         self._animate()
 
     def _animate(self):
+        if self._win is None:
+            return
         self._win.redraw()
         time.sleep(0.05)
